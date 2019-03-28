d1741f1ee7f21659056d7420184c86cc87854657
Casey <caseyclinga@gmail.com>
2019-03-28 14:02:00 -0700

    3.28 update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,23 +4,22 @@ var gameObject = {
     questions: {
         firstQuestion: {
             prompt: "Which of the following is NOT a white wine?",
-                correct: "Pinot noir",
-                wrong: {
-                    firstWrong: "Chardonnay",
-                    secondWrong: "Pinot gris",
-                    thirdWrong: "Pinot grigio"
-                }    
+            correct: "Pinot noir",
+            wrong: {
+                firstWrong: "Chardonnay",
+                secondWrong: "Pinot gris",
+                thirdWrong: "Pinot grigio"
             }
-        },  
+        },
         secondQuestion: {
             prompt: "Which spirit must have a grain mash of at least %51 corn?",
-                correct: "Bourbon",
-                wrong: {
-                    firstWrong: "Gin",
-                    secondWrong: "Rye",
-                    thirdWrong: "Rum"
+            correct: "Bourbon",
+            wrong: {
+                firstWrong: "Gin",
+                secondWrong: "Rye",
+                thirdWrong: "Rum"
             }
-        },  
+        },
         thirdQuestion: {
             prompt: "What iconic American city was the Sazerac invented in?",
             correct: "New Orleans",
@@ -29,8 +28,8 @@ var gameObject = {
                 secondWrong: "Seattle",
                 thirdWrong: "San Fransisco"
             }
-        },  
-        fourthQuestion: { 
+        },
+        fourthQuestion: {
             prompt: "The smokey taste in Islay Scotches comes from which smoked grain?",
             correct: "Malted barley",
             wrong: {
@@ -38,66 +37,141 @@ var gameObject = {
                 secondWrong: "Wheat",
                 thirdWrong: "Oats"
             }
-        }  
+        }
     }
-
-console.log(gameObject)
-console.log(gameObject.questions)
-console.log(gameObject.questions[0])
-console.log(gameObject.questions.firstQuestion.prompt)
-console.log(gameObject.questions.firstQuestion.correct)
-console.log(gameObject.questions.firstQuestion.answers)
-console.log(gameObject.questions.firstQuestion.wrong)
+}
 
 //Create variable to hold timer time
-var timeRemaining = 20;
+var timeRemaining = 5;
+
+//Create variable to hold score for correct, wrong guesses and ran-out-of-time
+var correctGuess = 0;
+var wrongGuess = 0;
+var runOutOfTime = 0;
+
+//Create variable to hold the current question
+var currentQuestion = [
+gameObject.questions.firstQuestion, gameObject.questions.secondQuestion, gameObject.questions.thirdQuestion, gameObject.questions.fourthQuestion
+];
+
+var timer; 
+
+//Make a function that stops the timer
+function stopTimer() {
+    clearInterval(timer);
+}
+ //Set up a function for when a wrong guess has been entered
+function incorrectChoice() {
+    $("#question").text("not even close")
+    $("#image").prepend('<img id="theImg" src="assets/images/bigstock-Senior-Man-With-Cough-2429487" />')
+    $(".guessButton").hide();
+    stopTimer();
+    wrongGuess++;
+    $("#scoreCounter").html('Correct: ' + correctGuess + ' ' + 'Wrong: ' + wrongGuess + ' ' + 'Out of time: ' + runOutOfTime);
+}
+
+//Set up a function for when the correct guess has been made
+function correctChoice() {
+    $("#question").text("correct!")
+        $("#image").prepend('<img id="theImg" src="assets/images/bigstock-Senior-Man-With-Cough-2429487" />')
+        $(".guessButton").hide();
+        stopTimer();
+        correctGuess++;
+        $("#scoreCounter").html('Correct: ' + correctGuess + ' ' + 'Wrong: ' + wrongGuess + ' ' + 'Out of time: ' + runOutOfTime);
+}
+    
+//Set up a function for when the user is out of time
+function outOfTime() {
+    $("#question").text("Out of time!")
+        $("#image").prepend('<img id="theImg" src="assets/images/bigstock-Senior-Man-With-Cough-2429487" />')
+        $(".guessButton").hide();
+        stopTimer();
+        runOutOfTime++;
+        $("#scoreCounter").html('Correct: ' + correctGuess + ' ' + 'Wrong: ' + wrongGuess + ' ' + 'Out of time: ' + runOutOfTime);
+}
+
+//Make a function that evalutes the time remaining and counts down 
+function countdown() {
+    if (timeRemaining === 0) {
+       outOfTime();
+       setTimeout(nextQuestion, 4000)
+    } else {
+        $("#timer").text(timeRemaining -= 1);
+    }
+}
+    
+//Set up a function that displays the next question
+function nextQuestion () {
+    $("#scoreCounter").hide();
+    $("#image").hide();
+    $(".guessButton").show();
+    timeRemaining = 21;
+    setInterval(countdown, 1000);
+    setUpNewQuestion(gameObject.questions.secondQuestion);
+}
 
 //Display a trivia game screen with a start button
-$(document).ready(function() {
-    $("button").on("click", function() {
+$(document).ready(function () {
+
+    //Hide the players choices
+    $("#buttonContainer").hide();
+
+    $("#start").on("click", function () {
 
         //Hide the start button after it is clicked
-        $("button").hide();
+        $("#start").hide();
         $("#timer").text(timeRemaining);
-        //Set the text of the timer element to the value of timeRemaining  
-        //set a timer to decrease the time remaining by one every second
-        setInterval(function(){
-            $("#timer").text(timeRemaining -= 1)}, 1000);
 
+        //Set a timer to set the text of the timer element to the value of timeRemaining and decrease the 
+        //... time remaining by one for every one second
+        timer = setInterval(countdown, 1000);
+
+        //Display the buttons  for the choices you have to make in the game
+        $("#buttonContainer").show();
+
+        setUpNewQuestion(gameObject.questions.firstQuestion);
+
+    });
+    
+    function setUpNewQuestion(selectedQuestion) {
         //Hold value of the question prompt in a variable 
         //Input that into the question element 
-        var questionInput = gameObject.questions.firstQuestion.prompt;
+        var questionInput = selectedQuestion.prompt;
         $("#question").text(questionInput);
-        
-        //Create a variable which will hold all possible answer choices
-        var choices = gameObject.questions.firstQuestion.correct + ' ';
+
+        //Create an array which will hold all possible answer choices
+        var choices = [selectedQuestion.correct];
         var x;
-        for(x in gameObject.questions.firstQuestion.wrong) {
-            choices += gameObject.questions.firstQuestion.wrong[x] + ' ';
+        for (x in selectedQuestion.wrong) {
+            choices.push(selectedQuestion.wrong[x]);
         }
 
-        //Set the text of the choices element to the variable holding all possible choices
-        $("#choices").text(choices);
+        //Shuffle the array
+        var choicesMixed = choices.sort();
+        assignButtonVal(choicesMixed);
+    }
 
-        console.log(choices)
-        console.log(timeRemaining);
-    });
+    function assignButtonVal(choicesMixed) {
+        $("#firstChoice").val(choicesMixed[0])
+        $("#secondChoice").val(choicesMixed[1])
+        $("#thirdChoice").val(choicesMixed[2])
+        $("#fourthChoice").val(choicesMixed[3])
+    }
 
+    $(".guessButton").on("click", function () {
 
-        
+        //If the player clicks the wrong option, switch to the incorrect guess screen and then switch to the next question after four seconds
+        //---Add one point on to the wrongGuess variable
+        if (this.value === currentQuestion[0].correct) {
+            correctChoice();
+            setTimeout(nextQuestion, 4000)
+        //If the player clicks the right option, switch to the correct guess screen and then switch to the next question after four seconds
+        //---Add one point on to the correctGuess variable
+        } else {
+            incorrectChoice();
+            console.log(wrongGuess);
+            setTimeout(nextQuestion, 4000)
+        }
+    })
 
-//If the timer runs to zero seconds, switch to the out of time screen and then switch to the next question after four seconds
-
-//If the player clicks the wrong option, switch to the incorrect guess screen and then switch to the next question after four seconds
-//---Add the wrong guess to a variable
-
-//If the player clicks the right option, switch to the correct guess screen and then switch to the next question after four seconds
-//---Add the right guess to a variable
-
-//After all questions have been run, switch to the end screen
-//---Display correct guesses, incorrect guesses and unanswered
-//---Have a start over button that starts the game over without refreshing the page
-
-   
 });
-
